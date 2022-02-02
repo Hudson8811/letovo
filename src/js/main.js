d7db075b2b16f60b53e50bfa7a81583a8cfb0f5c
TroshinSerg <serge-troshin@yandex.ru>
2022-02-02 18:22:51 +0300

    current state

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 //= ./../../node_modules/jquery/dist/jquery.min.js
 
 
+//= ./libs/scrolloverflow.min.js
 //= ./libs/fullpage.js
 
 
--- src/js/partials/main.js
+++ src/js/partials/main.js
@@ -16,53 +16,97 @@ window.addEventListener('load', () => {
   const results = [
     {
       title: 'Коммуникация',
-      text: 'Универсальный метапредметный — и бытовой — навык. Умение налаживать связи с окружающими, чувствовать и узнавать настрой и мнение окружающих — все это важно в любой сфере деятельности. В школе «Летово» коммуникация (кстати, билингвальная!) — одна из основ учебного процесса. Отсутствие классов, широкая и разнообразная сеть взаимодействий, множество групповых форм работы помогают ребенку найти комфортное место в коллективе.'
+      text: 'Универсальный метапредметный — и бытовой — навык. Умение налаживать связи с окружающими, чувствовать и узнавать настрой и мнение окружающих — все это важно в любой сфере деятельности. В школе «Летово» коммуникация (кстати, билингвальная!) — одна из основ учебного процесса. Отсутствие классов, широкая и разнообразная сеть взаимодействий, множество групповых форм работы помогают ребенку найти комфортное место в коллективе.',
+      image: {
+        name: 'communication.svg',
+        width: '705',
+        height: '624'
+      },
+      mod: 'communication'
     },
     {
       title: 'Сотрудничество',
-      text: 'Как часто можно слышать о специалисте: «Профессионал хороший, но работать с ним — ни за что!» Сегодня, когда почти все начинания требуют работы в коллективе, когда самая блестящая идея не может быть реализована без вклада множества специалистов, умение сотрудничать — один из самых насущных навыков. Командная работа — одна из сильных сторон «Летово»: ученики учатся сотрудничеству, работая в школьном самоуправлении, создавая социальные и благотворительные проекты. '
+      text: 'Как часто можно слышать о специалисте: «Профессионал хороший, но работать с ним — ни за что!» Сегодня, когда почти все начинания требуют работы в коллективе, когда самая блестящая идея не может быть реализована без вклада множества специалистов, умение сотрудничать — один из самых насущных навыков. Командная работа — одна из сильных сторон «Летово»: ученики учатся сотрудничеству, работая в школьном самоуправлении, создавая социальные и благотворительные проекты.',
+      image: {
+        name: 'cooperation.svg',
+        width: '657',
+        height: '590'
+      },
+      mod: 'cooperation'
     },
     {
       title: 'Самоорганизация',
-      text: 'Именно тот метапредметный навык, который «проседает» даже у взрослых людей — и спросите, рады ли они этому? Самоорганизация — залог не только успешной работы, но и гармоничной жизни. Школьники в «Летово» учатся самоорганизации непрерывно, составляя индивидуальный учебный план, отслеживая поставленные перед собой цели, стараясь успеть на все виды активностей, предлагаемые школой, от исторического фехтования до робототехники. Да и жизнь в школе без звонков (а в «Летово» их нет не случайно) способствует развитию организованности.'
+      text: 'Именно тот метапредметный навык, который «проседает» даже у взрослых людей — и спросите, рады ли они этому? Самоорганизация — залог не только успешной работы, но и гармоничной жизни. Школьники в «Летово» учатся самоорганизации непрерывно, составляя индивидуальный учебный план, отслеживая поставленные перед собой цели, стараясь успеть на все виды активностей, предлагаемые школой, от исторического фехтования до робототехники. Да и жизнь в школе без звонков (а в «Летово» их нет не случайно) способствует развитию организованности.',
+      image: {
+        name: 'self-organization.svg',
+        width: '510',
+        height: '532'
+      },
+      mod: 'self-organization'
     },
     {
       title: 'Исследование',
-      text: 'Стремление «дойти до самой сути» явления ценится во всех профессиональных сферах. Умение поставить исследовательский вопрос, составить план работы и представить результат в «Летово» оттачивается за дискуссионным столом и в химических лабораториях, на выступлениях в формате TED и даже... в спортивных залах. Это не шутка, ведь исследовательский подход важен и в спорте, и в творчестве, и в научной деятельности — а ученики «Летово» должны попробовать себя во всем перечисленном.'
+      text: 'Стремление «дойти до самой сути» явления ценится во всех профессиональных сферах. Умение поставить исследовательский вопрос, составить план работы и представить результат в «Летово» оттачивается за дискуссионным столом и в химических лабораториях, на выступлениях в формате TED и даже... в спортивных залах. Это не шутка, ведь исследовательский подход важен и в спорте, и в творчестве, и в научной деятельности — а ученики «Летово» должны попробовать себя во всем перечисленном.',
+      image: {
+        name: 'study.svg',
+        width: '468',
+        height: '560'
+      },
+      mod: 'study'
     },
     {
       title: 'Критическое мышление',
-      text: 'Ежечасно на нас обрушивается поток информации, и его плотность продолжает нарастать. Так же будет нарастать «спрос» на умение работать с этой информацией: отделять достоверное, анализировать, использовать в своей деятельности. В «Летово» критическое мышление особенно востребовано в ходе работы над проектами: ученики школы создают востребованные на рынке устройства для исправления осанки, запускают масштабные образовательные проекты, разрабатывают мобильные приложения — и, конечно же, успешно и увлеченно учатся.'
+      text: 'Ежечасно на нас обрушивается поток информации, и его плотность продолжает нарастать. Так же будет нарастать «спрос» на умение работать с этой информацией: отделять достоверное, анализировать, использовать в своей деятельности. В «Летово» критическое мышление особенно востребовано в ходе работы над проектами: ученики школы создают востребованные на рынке устройства для исправления осанки, запускают масштабные образовательные проекты, разрабатывают мобильные приложения — и, конечно же, успешно и увлеченно учатся.',
+      image: {
+        name: 'critical-thinking.svg',
+        width: '311',
+        height: '653'
+      },
+      mod: 'critical-thinking'
     }
   ];
 
   const heroButton = document.querySelector('.hero__btn');
-  const getResultButton = document.querySelector('.choice__btn');
+  const choiceSection = document.querySelector('.choice');
+  const showResultButton = choiceSection.querySelector('.choice__btn');
   const resultSection = document.querySelector('.result');
   const chList = document.querySelector('.characteristics');
-  const resultShortSpan = document.querySelector('.result__short span');
-  const resultText = document.querySelector('.result__text');
-  const resultForm = document.querySelector('.result form');
-  const resultFormNotify = resultForm.querySelector('.notify');
-  const resultFormBtn = resultForm.querySelector('button[type="submit"]');
+  const resultTitle = document.querySelector('.result__title');
+  const resultText = document.querySelector('.result__main-text');
+  const resultImageWrapp = document.querySelector('.result__image');
+
+  const resultResetButton = resultSection.querySelector('.__js_reset-result');
+
+  let isMobile = document.documentElement.clientWidth <= 800;
+  let isDesktopFullpageActive = !isMobile;
+  let isMobileFullpageActive = isMobile;
+  let isReset = false;
+  let fp = null;
+  const scrollingSpeed = 700;
 
   const fpOptions = {
     licenseKey: '930B3D8E-64114A48-BE58EB40-E2698A87',
     autoScrolling: true,
     scrollHorizontally: false,
     verticalCentered: false,
-    afterReBuild: () => {
-      fp.moveSectionDown();
+    scrollOverflow: isMobile,
+    scrollingSpeed: scrollingSpeed,
+    afterLoad: function() {
+      if (isReset) {
+        /*resultSection.className = 'result hide';
+        resultTitle.textContent = '';
+        resultText.textContent = '';
+        resultImageWrapp.innerHTML = '';
+        fp.destroy('all');
+        choiceSection.classList.add('active')
+        fp = new fullpage('#fullpage', fpOptions);
+
+        isReset = false;*/
+      }
     }
   };
 
-  const fp = new fullpage('#fullpage', fpOptions);
-
-  const ErrorMessage = {
-    EMPTY: 'Поле обязательно для заполнения.',
-    AGE: 'Допускается значение от 3 до 18 лет',
-    EMAIL: 'Введите корректный email'
-  };
+  fp = new fullpage('#fullpage', fpOptions);
 
   const modClass = 'characteristics__item--selected';
   const maxSelected = 3;
@@ -70,28 +114,37 @@ window.addEventListener('load', () => {
 
   let selectedIds = [];
 
-  const postData = (body) => {
-    return fetch('/save_form/', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(body),
-    });
+  const reinit = (section) => {
+    fp.destroy('all');
+    section.classList.add('active')
+    fp = new fullpage('#fullpage', fpOptions);
   };
 
-  const isNameFieldValid = () => {
-    return resultForm.name.value !== '';
-  };
+  const debounce = function(func, wait, immediate) {
+    let timeout;
 
-  const isAgeFieldValid = () => {
-    const value = parseInt(resultForm.age.value, 10);
-    return value !== '' && (value >= 3 && value < 18);
-  };
+    return function executedFunction() {
+      const context = this;
+      const args = arguments;
 
-  const isEmailFieldValid = () => {
-    return resultForm.email.value !== '' && resultForm.email.value.match(
-    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+      const later = function() {
+        timeout = null;
+
+        if (!immediate) {
+          func.apply(context, args);
+        }
+      };
+
+      const callNow = immediate && !timeout;
+
+      clearTimeout(timeout);
+
+      timeout = setTimeout(later, wait);
+
+      if (callNow) {
+        func.apply(context, args);
+      }
+    };
   };
 
   const getDublicate = (arr) => {
@@ -111,34 +164,125 @@ window.addEventListener('load', () => {
 
     const index = (setFromIndexes.size === indexes.length) ? indexes[0] : getDublicate(indexes);
 
-    resultShortSpan.textContent = results[index - 1].title;
-    resultText.textContent = results[index - 1].text;
+    const resultItem = results[index - 1];
+
+    const image = new Image();
+    image.src = `images/${resultItem.image.name}`;
+    image.width = resultItem.image.width;
+    image.height = resultItem.image.height;
+
+  
+    resultSection.classList.remove('hide');
+    resultSection.classList.add('section', `result--${resultItem.mod}`);
+
+
+    resultTitle.textContent = resultItem.title;
+    resultText.textContent = resultItem.text;
+    resultImageWrapp.appendChild(image);
   };
 
-  const createErrorMessageElement = (message) => {
-    const element = document.createElement('span');
-    element.classList.add('message');
-    element.textContent = message;
-    return element;
+  const showResult = (e) => {
+    e.preventDefault();
+    
+    if (selectedCount === maxSelected) {
+      const activeSection = document.querySelector('.section.active');
+
+      showResultButton.onclick = resetResult;
+      resultResetButton.onclick = resetResult;
+      chList.classList.add('blocked');
+
+      setResult();
+
+      fp.destroy('all');
+      activeSection.classList.add('active');
+      fp = new fullpage('#fullpage', fpOptions);
+      fp.moveSectionDown();
+
+      setTimeout(() => {
+        showResultButton.textContent = 'Выбрать еще раз';
+      }, scrollingSpeed);
+    }
   };
 
-  const changeValidityState = (input, message, isValid) => {
-    const parent = input.parentElement;
-    const messageElement = parent.querySelector('.message');
+  const resetResult = (e) => {
+    e.preventDefault();
 
-    parent.classList[isValid ? 'remove' : 'add']('error');
+    const reset = () => {
+      resultSection.className = 'result hide';
+      resultTitle.textContent = '';
+      resultText.textContent = '';
+      resultImageWrapp.innerHTML = '';
 
-    if (isValid && messageElement !== null) {
-      messageElement.remove();
+      reinit(choiceSection);
+    };
+
+    showResultButton.textContent = 'Получить результат';
+    showResultButton.onclick = showResult;
+    resultResetButton.onclick = null;
+
+    chList.classList.remove('blocked', 'done');
+    selectedCount = 0;
+    chList.querySelectorAll('.characteristics__item').forEach((it) => it.classList.remove(modClass));
+
+    if (e.target === resultResetButton) {
+      fp.moveSectionUp();
+      setTimeout(reset, scrollingSpeed);
+    } else {
+      reset();
+    }
+    
+  };
+
+  const reinitFullpage = debounce(() => {
+    const activeSection = document.querySelector('.section.active');
+
+    if (isMobile && !isMobileFullpageActive) {
+      isMobileFullpageActive = true;
+      isDesktopFullpageActive = false;
+      reinit(activeSection);
     }
 
-    if (!isValid && messageElement === null) {
-      parent.appendChild(createErrorMessageElement(message));
+    if (!isMobile && !isDesktopFullpageActive) {
+      isMobileFullpageActive = false;
+      isDesktopFullpageActive = true;
+      reinit(activeSection);
     }
-  };
+  }, 50, false);
 
+  // Только для тестирования
+  window.createResult = (num) => {
+    const activeSection = document.querySelector('.section.active');
+      fp.destroy('all');
+    
+      ////////////////////////////////////
+    const resultItem = results[num - 1];
+    const image = new Image();
+    image.src = `images/${resultItem.image.name}`;
+    image.width = resultItem.image.width;
+    image.height = resultItem.image.height;
+
+    resultSection.classList.remove('hide');
+    resultSection.classList.add('section', `result--${resultItem.mod}`);
+
+    resultTitle.textContent = resultItem.title;
+    resultText.textContent = resultItem.text;
+    resultImageWrapp.innerHTML = '';
+    resultImageWrapp.appendChild(image);
+    
+    ///////////////////////////////
+    activeSection.classList.add('active');
+    fp = new fullpage('#fullpage', fpOptions);
+    fp.moveSectionDown();
+  };
 
   ////////////////////////////////////////////////////
+
+  window.addEventListener('resize', () => {
+    isMobile = document.documentElement.clientWidth <= 800;
+    fpOptions.scrollOverflow = isMobile;
+    reinitFullpage();
+  });
+
 
   chList.onclick = (e) => {
     const item = e.target.closest('.characteristics__item');
@@ -153,111 +297,27 @@ window.addEventListener('load', () => {
         selectedIds.push(itemNum);
       }
 
-      if (selectedCount === maxSelected) {
-        item.blur();
-      }
-
       if (isSelected) {
         item.classList.remove(modClass);
+        item.blur();
         selectedCount--;
         selectedIds = selectedIds.filter((it) => it !== itemNum);
+      }
+
+      if (selectedCount === maxSelected) {
+        item.blur();
+        chList.classList.add('done');
+      } else {
+        chList.classList.remove('done');
       }
     }
   };
 
   heroButton.onclick = (e) => {
     e.preventDefault();
-    fullpage_api.moveTo(2);
+    fp.moveTo(2);
   };
 
-  getResultButton.onclick = (e) => {
-    e.preventDefault();
-    
-    if (selectedCount === maxSelected) {
-      resultSection.classList.remove('hide');
-      fp.reBuild();
-      setResult();
-    }
-  };
-
-  resultForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    const isNameValid = isNameFieldValid();
-    const isAgeValid = isAgeFieldValid();
-    const isEmailValid = isEmailFieldValid();
-
-    if (isNameValid && isAgeValid && isEmailValid) {
-      const btnText = resultFormBtn.textContent;
-      const formData = new FormData(resultForm);
-      const body = {};
-
-      for (let element of resultForm.elements) {
-        element.disabled = true
-      }
-      resultFormBtn.textContent = 'Отправка заявки...';
-      resultFormNotify.textContent = '';
-      resultFormNotify.className = 'notify';
-
-      formData.forEach((value, key) => {
-        body[key] = value;
-      });
-      
-      postData(body)
-        .then(response => {
-          if (response.status !== 200) {
-            throw new Error('Network status is not 200');
-          }
-          return response.json();
-        })
-        .then(data => {
-          if (data.error_no === 0) {
-            for (let element of resultForm.elements) {
-              element.disabled = false
-            }
-            resultFormBtn.textContent = btnText;
-            resultFormNotify.classList.add('success');
-            resultFormNotify.textContent = 'Ваша заявка успешно отправлена!';
-
-            setTimeout(() => {
-              resultFormNotify.textContent = '';
-              resultFormNotify.className = 'notify';
-            }, 3000);
-          } else if (data.error_no > 0) {
-            for (let element of resultForm.elements) {
-              element.disabled = false
-            }
-            resultFormBtn.textContent = btnText;
-            resultFormNotify.classList.add('error');
-            resultFormNotify.textContent = data.error_text;
-
-            setTimeout(() => {
-              resultFormNotify.textContent = '';
-              resultFormNotify.className = 'notify';
-            }, 3000);
-          }
-        })
-        .catch(error => {
-          console.error(error);
-
-          for (let element of resultForm.elements) {
-            element.disabled = false
-          }
-          resultFormBtn.textContent = btnText;
-          resultFormNotify.classList.add('error');
-
-          setTimeout(() => {
-            resultFormNotify.textContent = '';
-            resultFormNotify.className = 'notify';
-          }, 3000);
-        });
-    } else {
-      changeValidityState(resultForm.name, ErrorMessage.EMPTY, isNameValid);
-      changeValidityState(resultForm.age, ErrorMessage.AGE, isAgeValid);
-      changeValidityState(resultForm.email, ErrorMessage.EMAIL, isEmailValid);
-    }
-
-    
-
-  });
+  showResultButton.onclick = showResult;
+  resultResetButton.onclick = resetResult;
 });
